1a563a53037845b97d3fb85d8ec67b78a69639be
Juan Luna <juanfranlunapr@gmail.com>
2025-10-15 15:15:47 -0600

    C.o

--- Portfolio/05-JS/js/fibonacci.js
+++ Portfolio/05-JS/js/fibonacci.js
@@ -7,7 +7,9 @@ var memo = {};
 function fibonacci() {
   "use strict";
   var n = document.getElementById("num").value;
+  console.log(n)
   var val = f(n);
+  document.getElementById("fibonacciLbl").innerText = val
   return val;
 }
 
@@ -17,11 +19,15 @@ function f(n) {
   if (memo.hasOwnProperty(n)) {
     value = memo[n];
   } else {
-    //TODO: Implement the fibonacci function here!
-
+if (n <= 1) {
+      value = n;
+    } else {
+      value = f(n - 1) + f(n - 2);
+    }
     memo[n] = value;
   }
 
   return value;
 }
+
 console.log(fibonacci(15));
--- Portfolio/05-JS/js/sieve_eratosthenes.js
+++ Portfolio/05-JS/js/sieve_eratosthenes.js
@@ -1,9 +1,4 @@
-/*
-    Sieve of Eratosthenes - The sieve of Eratosthenes is one of the most efficient ways
-    to find all of the smaller primes (below 10 million or so).
-*/
 
-// TODO: Adjust this script so it can work with the sieve.html file.
 
 var sieve = function (n) {
   "use strict";
@@ -13,9 +8,34 @@ var sieve = function (n) {
     i,
     j;
 
-  // TODO: Implement the sieve of eratosthenes algorithm to find all the prime numbers under the given number.
+
+  for (i = 2; i <= n; i++) {
+    array[i] = true;
+  }
+
+  for (i = 2; i * i <= n; i++) {
+    if (array[i]) {
+      for (j = i * i; j <= n; j += i) {
+        array[j] = false;
+      }
+    }
+  }
+
+  for (i = 2; i <= n; i++) {
+    if (array[i]) primes.push(i);
+  }
 
   return primes;
 };
 
-console.log(sieve(1000000));
+document.getElementById("btn").addEventListener("click", function () {
+  var num = parseInt(document.getElementById("num").value);
+  if (isNaN(num) || num < 2) {
+    document.getElementById("primes").textContent = "Please enter a number greater than 1.";
+    return;
+  }
+  var primes = sieve(num);
+  document.getElementById("primes").textContent = primes.join(", ");
+});
+
+
